77a35b185b98d481ca84c6141b31cb616ded53e0
GUOQING LI <leevn2011@hotmail.com>
2018-03-28 21:41:35 +0700

    test and update

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -1,13 +1,17 @@
 package camt.se234.lab10.service;
 
-import camt.se234.lab10.entity.Grade;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GradeServiceImpl implements GradeService {
     @Override
-    public String getGrade(double score) {  
-            return "F";
+    public String getGrade(double score) {
+     if(score>=80 && score<=100) return "A";
+     else if (score>=75 && score<80) return "B";
+     else if (score>=60 && score<75) return "C";
+     else if (score>=33 && score<60) return "D";
+     else if (score<33 && score>=0)  return "F";
+     else return "Invalid input";
 
     }
 }
